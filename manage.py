89a51ff85ecb124f7ea3f17b75456bde94bc2ea1
jmreesjo <jmreesjo@ncsu.edu>
2016-04-10 12:19:09 -0400

    Add code coverage for testing

--- manage.py
+++ manage.py
@@ -1,5 +1,11 @@
 import os
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='wolfstudy/*')
+    COV.start()
+
 if os.path.exists('.env'):
     print ('Importing environment from .env...')
     for line in open('.env'):
@@ -40,9 +46,23 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test(coverage=False):
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print 'Coverage summary:'
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print 'HTML version: file://%s/index.html' % covdir
+        COV.erase()
 
 
 
